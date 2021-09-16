20f296d99347fabe2789019f74dcd754fd1fcf7c
mogil <webmogilevtsev@ya.ru>
2021-09-16 14:44:18 +0500

    style(code): change style
    change code style by .eslintrc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common'
 import { AppService, ICategory, IItem } from './app.service'
 
 @Controller()
@@ -7,12 +7,16 @@ export class AppController {
 
   @Get('items')
   async getItems(): Promise<IItem[]> {
-    return await this.appService.getItems('https://greenwaystart.com/products/iGen/')
+    return await this.appService.getItems(
+      'https://greenwaystart.com/products/iGen/',
+    )
   }
 
   @Get('item-info')
   async getItemInfo(): Promise<any[]> {
-    return await this.appService.getItemInfo('https://greenwaystart.com/products/iGen/05101/')
+    return await this.appService.getItemInfo(
+      'https://greenwaystart.com/products/iGen/05101/',
+    )
   }
 
   @Get('categories')
