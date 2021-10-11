104b9b99b53502d667f9fa96a2c6c805e913438f
Mogilevtsev Dmitry <webmogilevtsev@ya.ru>
2021-10-11 23:42:04 +0500

    add nodemailer.module.ts

---
+++ src/nodemailer/nodemailer.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common'
+import * as nodemailer from 'nodemailer'
+import { ConfigService } from '@nestjs/config'
+
+export interface IMailMessage {
+  to: string
+  subject: string
+  text?: string
+  from?: string
+  html?: string
+}
+
+@Injectable()
+export class NodemailerService {
+  private transporter: any
+
+  constructor(
+    private readonly configService: ConfigService,
+  ) {
+  }
+
+  private async createTransporter() {
+    this.transporter = nodemailer.createTransport({
+      host: this.configService.get<string>('MAIL_HOST'),
+      port: this.configService.get<number>('MAIL_PORT'),
+      secure: false,
+      requireTLS: true,
+      auth: {
+        user: this.configService.get<string>('MAIL_USER'),
+        pass: this.configService.get<string>('MAIL_PASSWORD'),
+      },
+    })
+  }
+
+  public async sendMessage(message: IMailMessage) {
+    await this.createTransporter()
+    message.from = this.configService.get<string>('MAIL_USER')
+    await this.transporter.sendMail(message, (e, i) => {
+      if (e) {
+        throw new Error(e)
+      } else {
+        console.log('Email sent: ' + i.response)
+      }
+    })
+    this.transporter.close()
+  }
+}
+
